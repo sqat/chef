7619b7e0aae2f51525ba56c9460c50103b3c20fb
Tenant10 tenant10 <tenant10@sqat-core-sotfservice.tnt10-zone1.aus1>
2013-09-03 06:47:53 -0700

    made changes to scripts

--- cookbooks/sqat/recipes/default.rb
+++ cookbooks/sqat/recipes/default.rb
@@ -1,3 +1,6 @@
+require 'socket'
+node.default["system"]["host"]=Socket.gethostname
+node.default["p4settings"]["P4CLIENT"]=Socket.gethostname+".tnt10"
 
 directory "#{node[:home]}#{node[:programs]}" do
   owner "#{node[:system][:owner]}"
@@ -43,21 +46,21 @@ cookbook_file "#{node[:home]}#{node[:programs]}#{node[:perforce]}/bin/p4" do
   source "p4"
 end
 
-directory "#{node[:home]}#{node[:workspace]}" do
+directory "#{node[:workspace]}" do
   owner "#{node[:system][:owner]}"
   mode "0775"
   recursive true
   action :create
 end
 
-template "#{node[:home]}#{node[:workspace]}/p4client" do
+template "#{node[:workspace]}/p4client.txt" do
   owner "#{node[:system][:owner]}"
   user "#{node[:system][:owner]}"
   mode "0644"
   source "p4client.erb"
   variables(
     :P4CLIENT => node[:p4settings][:P4CLIENT],
-    :WORKSPACE => "#{node[:home]}#{node[:workspace]}",
+    :WORKSPACE => "#{node[:workspace]}",
     :P4USER => node[:p4settings][:P4USER],
     :HOST => "#{node[:system][:host]}"
   )
@@ -74,5 +77,34 @@ template "#{node[:home]}/.bashrc" do
   )
 end
 
+ruby_block "p4login" do
+ block do
+  require "P4"
+  p4 = P4.new
+  p4.port="#{node[:p4settings][:P4PORT]}"
+  p4.user="#{node[:p4settings][:P4USER]}"
+  password=''
+  File.open("#{node[:home]}/.p4passwd").each{|line| password << line}
+  p4.password = password
+  p4.connect
+  p4.run_login
+  text=''
+  File.open("#{node[:workspace]}/p4client.txt").each { |line| text << line }
+  p4.input=text
+  p4.run_client("-i")
+  p4.client="#{node[:p4settings][:P4CLIENT]}"
+  begin
+   p4.run_sync
+  rescue
+   puts "Files up to date"
+  end
 
+ end
+end
 
+script "copy p4 ticket" do
+interpreter "bash"
+code <<-EOH
+ cp /root/.p4tickets /home/tenant10/.p4tickets
+EOH
+end
--- cookbooks/sqat/solo.rb
+++ cookbooks/sqat/solo.rb
@@ -2,4 +2,3 @@
   file_cache_path "/home/tenant10/Temp"
   cookbook_path "/home/tenant10/workarea/chef/cookbooks"
   json_attribs "/home/tenant10/workarea/chef/cookbooks/sqat/node.json"
-
