8e917c190e6270de64bcc2493439400044355b81
sqat <snapfish-sqat@valuelabs.com>
2014-02-17 17:15:03 +0530

    Update vmware.sh

--- bin/vmware.sh
+++ bin/vmware.sh
@@ -1,1 +1,1 @@
-ruby vmware.rb --host vcenter1.op-zone1.aus1 --user tenant10 --password Sfq1test1 --insecure --datacenter MP1 $2 $1.tnt13-zone1.aus1 tenant10/test
+ruby vmware.rb --host vcenter1.op-zone1.aus1 --user sqat --password ***sqatpswd*** --insecure --datacenter MP1 $2 $1 tenant26/sqat/$3
