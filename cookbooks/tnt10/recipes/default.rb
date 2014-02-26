e9f8e009a2bfb115dfb7ca5d410de1ab624b07a7
sqat <snapfish-sqat@valuelabs.com>
2014-02-26 11:42:09 +0530

    Update default.rb

--- cookbooks/tnt10/recipes/default.rb
+++ cookbooks/tnt10/recipes/default.rb
@@ -159,7 +159,17 @@ elsif workarea.eql?("buildbox")
   )
 end
 
+execute "-- creating .chef folder " do
+  user "#{node[:system][:owner]}"
+  cwd "#{node[:home]}"
+  command "mkdir #{node[:home]}/#{node[:build_chef]}"
+end
 
+execute "-- copying files " do
+  user "#{node[:system][:owner]}"
+  cwd "#{node[:home]}/#{node[:build_chef]}"
+  command "cp -a #{node[:home]}#{node[:build_bin]}/knife.rb #{node[:home]}#{node[:build_bin]}/vmware.sh #{node[:home]}#{node[:build_bin]}/vmware.rb #{node[:home]}/#{node[:build_chef]}/"
+end
 
 else
   puts "No p4client template taken" 
