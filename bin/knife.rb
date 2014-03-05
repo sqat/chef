130aa42fb1cff0b6abed7f57cbb46c7f97fd9f71
sqat <snapfish-sqat@valuelabs.com>
2014-03-05 19:45:47 +0530

    Update knife.rb

--- bin/knife.rb
+++ bin/knife.rb
@@ -2,6 +2,6 @@ knife[:vsphere_host] = "vcenter1.op-zone1.aus1"
 knife[:vsphere_insecure] = true
 knife[:vsphere_port] = 443
 knife[:vsphere_user] = "sqat"
-knife[:vsphere_pass] = "Sqataut06"
+knife[:vsphere_pass] = "xxxx"
 knife[:vsphere_dc] = "MP1"
 knife[:vsphere_resource_pool] = "DRS01/tenant26"
